e4f4b5b0e2e82152c2913f3936cbf9866d72f56c
Ubspy <jack-m@live.com>
2018-10-20 14:54:43 -0500

    Added blur lag to compensate for inaccurate recognition

--- faceRecognition.py
+++ faceRecognition.py
@@ -9,35 +9,80 @@ camera = cv2.VideoCapture(0)
 camera.set(3, 640)
 camera.set(4, 480)
 
+previousFrames = {'one': [], 'two': [], 'three': [], 'four': [], 'five': [], 'six': [], 'seven': [], 'eight': [], 'nine': [], 'ten': []}
+
 def blurArea(frame, x1, y1, x2, y2):
     frame[y1:y2, x2:x1] = cv2.blur(frame[y1:y2, x2:x1], (45, 45))
 
     for i in range(1, 15):
-        frame[y1-40:y2+40, x2-40:x1+40] = cv2.medianBlur(frame[y1-40:y2+40, x2-40:x1+40], 7)
+        frame[y1-20:y2+20, x2-20:x1+20] = cv2.medianBlur(frame[y1-20:y2+20, x2-20:x1+20], 7)
+
+def swapFrameObjects():
+    previousFrames['ten'] = previousFrames['nine']
+    previousFrames['nine'] = previousFrames['eight']
+    previousFrames['eight'] = previousFrames['seven']
+    previousFrames['seven'] = previousFrames['six']
+    previousFrames['six'] = previousFrames['five']
+    previousFrames['five'] = previousFrames['four']
+    previousFrames['four'] = previousFrames['three']
+    previousFrames['three'] = previousFrames['two']
+    previousFrames['two'] = previousFrames['one']
+    previousFrames['one'] = []
 
 def processFrame(frame):
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceRecognition.face_locations(grayFrame)
-    profiles = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+    profiles = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
 
     grayFrame = cv2.flip(grayFrame, 1)
-    profilesInv = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+    profilesInv = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
+
+    for (x1, y1, x2, y2) in previousFrames['one']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['two']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['three']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['four']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['five']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['six']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['seven']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['eight']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['nine']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    for (x1, y1, x2, y2) in previousFrames['ten']:
+        blurArea(frame, x1, y1, x2, y2)
+
+    swapFrameObjects()
 
     for (y1, x1, y2, x2) in faces:
         blurArea(frame, x1, y1, x2, y2)
+        previousFrames['one'].append((x1, y1, x2, y2))
 
     for (x, y, width, height) in profiles:
         blurArea(frame, x + width, y, x, y + height)
-        #frame[y:(y + height), x:(x + width)] = cv2.blur(frame[y:(y + height), x:(x + width)], (50, 50))
+        previousFrames['one'].append((x + width, y, x, y + height))
 
     for (x, y, width, height) in profilesInv:
         x = (640 - x)
         blurArea(frame, x, y, x - width, y + height)
-        #cv2.rectangle(frame, (x, y), (x - width, y + height), (0, 255, 0), 2)
-        #frame[y:(y + height), (x - width):x] = cv2.blur(frame[y:(y + height), (x - width):x], (50, 50))
+        previousFrames['one'].append((x, y, x - width, y + height))
 
     outputFrames.append(frame)
-
 
 profileCascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
