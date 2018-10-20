f3f03a355398feeb16fac014fb90c3e55ebd3e50
Ubspy <jack-m@live.com>
2018-10-20 15:15:06 -0500

    Moved unnecessary files into tmp python file, made py file more OO

--- faceDetection.py
+++ faceDetection.py
@@ -80,5 +80,33 @@ def processFrame(frame):
 
 profileCascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
 
+inputFrames = []
+outputFrames = []
+
+timeout = time.time() + 10
+
+while(True):
+    # Captures video for frame
+    ret, frame = camera.read()
+
+    inputFrames.append(frame)
+
+    if time.time() > timeout:
+        break
+
+camera.release()
+currentFrame = 0
+
+for frame in inputFrames:
+    print("{0}/{1}".format(currentFrame, len(inputFrames)))
+    processFrame(frame)
+    currentFrame = currentFrame + 1
+
+video = cv2.VideoWriter('gay.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 20, (640, 480))
+
+for frame in outputFrames:
+    video.write(frame)
+
 # When exited, it closes the stream
 cv2.destroyAllWindows()
+video.release()
