6b4d97e9e0eff8c910c5cdfaa711fc78885d8488
Ubspy <jack-m@live.com>
2018-10-20 10:06:11 -0500

    Adds blur to image

--- faceRecognition.py
+++ faceRecognition.py
@@ -11,24 +11,24 @@ camera.set(4, 480)
 
 def processFrame(frame):
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     faces = faceRecognition.face_locations(grayFrame)
-
     profiles = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
 
     grayFrame = cv2.flip(grayFrame, 1)
-
     profilesInv = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
 
     for (y1, x1, y2, x2) in faces:
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        frame[y1:y2, x2:x1] = cv2.blur(frame[y1:y2, x2:x1], (50, 50))
 
     for (x, y, width, height) in profiles:
-        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
+        frame[y:(y + height), x:(x + width)] = cv2.blur(frame[y:(y + height), x:(x + width)], (50, 50))
 
     for (x, y, width, height) in profilesInv:
         x = (640 - x)
-        cv2.rectangle(frame, (x, y), (x - width, y + height), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x - width, y + height), (0, 255, 0), 2)
+        frame[y:(y + height), (x - width):x] = cv2.blur(frame[y:(y + height), (x - width):x], (50, 50))
 
     outputFrames.append(frame)
 
