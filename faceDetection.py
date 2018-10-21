de847ec7970e9dc182c79405f53262c598c29dd4
Ubspy <jack-m@live.com>
2018-10-21 02:17:54 -0500

    I did a lot here, it is able to train models to recognize faces, and actually not blur them

--- faceDetection.py
+++ faceDetection.py
@@ -1,15 +1,28 @@
 import cv2
 import face_recognition as faceRecognition
 import numpy as np
+import pickle
 import time
 
+camera = cv2.VideoCapture(0)
+data = pickle.loads(open('encodings.xml', "rb").read())
+
+camera.set(3, 640) # set video widht
+camera.set(4, 480) # set video height
+
 previousFrames = {'one': [], 'two': [], 'three': [], 'four': [], 'five': [], 'six': [], 'seven': [], 'eight': [], 'nine': [], 'ten': []}
+faceIdentities = []
+
+profileCascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+
+inputFrames = []
+outputFrames = []
 
 def blurArea(frame, x1, y1, x2, y2):
-    frame[y1:y2, x2:x1] = cv2.blur(frame[y1:y2, x2:x1], (45, 45))
+    frame[y1:y2, x1:x2] = cv2.blur(frame[y1:y2, x1:x2], (45, 45))
 
     for i in range(1, 15):
-        frame[y1-20:y2+20, x2-20:x1+20] = cv2.medianBlur(frame[y1-20:y2+20, x2-20:x1+20], 7)
+        frame[y1-20:y2+20, x1-20:x2+20] = cv2.medianBlur(frame[y1-20:y2+20, x1-20:x2+20], 7)
 
 def swapFrameObjects():
     previousFrames['ten'] = previousFrames['nine']
@@ -23,14 +36,7 @@ def swapFrameObjects():
     previousFrames['two'] = previousFrames['one']
     previousFrames['one'] = []
 
-def processFrame(frame):
-    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceRecognition.face_locations(grayFrame)
-    profiles = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
-
-    grayFrame = cv2.flip(grayFrame, 1)
-    profilesInv = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
-
+def ghostBlur():
     for (x1, y1, x2, y2) in previousFrames['one']:
         blurArea(frame, x1, y1, x2, y2)
 
@@ -61,38 +67,79 @@ def processFrame(frame):
     for (x1, y1, x2, y2) in previousFrames['ten']:
         blurArea(frame, x1, y1, x2, y2)
 
+def matchFaces(frame, faces):
+    for face in faces:
+        encodings = faceRecognition.face_encodings(frame, [face])
+
+        for encoding in encodings:
+            matches = faceRecognition.compare_faces(data['encodings'], encoding)
+            name = "Unknown"
+
+        if True in matches:
+            matchedIndexes = [i for (i, b) in enumerate(matches) if b]
+            counts = {}
+
+            for currentMatch in matches:
+                if currentMatch:
+                    name = data['names'][matches.index(currentMatch)]
+                else:
+                    name = "Unknown"
+                counts[name] = counts.get(name, 0) + 1
+            name = max(counts, key=counts.get)
+
+        global faceIdentities
+        faceIdentities.append(name)
+
+def processFrame(frame):
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceRecognition.face_locations(grayFrame)
+    profiles = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
+
+    grayFrame = cv2.flip(grayFrame, 1)
+    profilesInv = profileCascade.detectMultiScale(grayFrame, scaleFactor=1.2, minNeighbors=5)
+
+    ghostBlur()
     swapFrameObjects()
+    matchFaces(frame, faces)
 
-    for (y1, x1, y2, x2) in faces:
-        blurArea(frame, x1, y1, x2, y2)
-        previousFrames['one'].append((x1, y1, x2, y2))
+    global faceIdentities
+    for (y1, x2, y2, x1) in faces:
+        if faceIdentities[faces.index((y1, x2, y2, x1))] == "Unknown":
+            blurArea(frame, x1, y1, x2, y2)
+            previousFrames['one'].append((x1, y1, x2, y2))
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    for (x, y, width, height) in profiles:
-        blurArea(frame, x + width, y, x, y + height)
-        previousFrames['one'].append((x + width, y, x, y + height))
-
-    for (x, y, width, height) in profilesInv:
-        x = (640 - x)
-        blurArea(frame, x, y, x - width, y + height)
-        previousFrames['one'].append((x, y, x - width, y + height))
-
+    faceIdentities = []
+    matchFaces(frame, profiles)
+    
     outputFrames.append(frame)
+    faceIdentities = []
 
-profileCascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+'''
+    for i, (x, y, width, height) in enumerate(profiles):
+        if faceIdentities[i] == "Unknown":
+            blurArea(frame, x, y, x + width, y + height)
+            previousFrames['one'].append((x, y, x + width, y + height))
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (255, 0, 0), 2)
 
-inputFrames = []
-outputFrames = []
+    faceIdentities = []
+    matchFaces(frame, profilesInv)
 
-timeout = time.time() + 10
+    for i, (x, y, width, height) in enumerate(profilesInv):
+        if faceIdentities[i] == "Unknown":
+            x = (640 - x)
+            blurArea(frame, x, y, x + width, y + height)
+            previousFrames['one'].append((x - width, y, x, y + height))
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 255), 2)
+'''
 
-while(True):
+timeout = time.time()
+
+while (time.time() - timeout) < 8:
     # Captures video for frame
     ret, frame = camera.read()
 
     inputFrames.append(frame)
-
-    if time.time() > timeout:
-        break
 
 camera.release()
 currentFrame = 0
